eee8e2effc1973c28e2ae9f78d994acb8628c642
Loris Cro <kappaloris@gmail.com>
2024-07-05 23:00:00 +0200

    update readme

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
 # SuperHTML
+Actually functional HTML Language Server (and Templating Language Library).
 
-A HTML templating language.
+**NOTE: SuperHTML is still incomplete, some features are missing and looking of somebody to implement them :^)**
 
-**Super is pre-alpha stage, using it now requires participating to its development**.
+## HTML Language Server
+The Super CLI Tool offers syntax checking and autoformatting features for HTML files.
 
-## Contributing to the HTML pasers & LSP
+```
+$ super
+Usage: super COMMAND [OPTIONS]
+
+Commands:
+  fmt          Format HTML documents
+  lsp          Start the Super LSP
+  help         Show this menu and exit
+
+General Options:
+  --help, -h   Print command specific usage  
+```
+
+### VSCode Support
+1. Download a prebuilt version of `super` from the Releases section (or build it yourself).
+2. Put `super` in your `PATH`.
+3. Install the [Super HTML VSCode extension](https://marketplace.visualstudio.com/items?itemName=LorisCro.super). 
+
+
+## Templating Language Library
+SuperHTML is not only an LSP but also an HTML templating language. More on that soon.
+
+## Contributing
+SuperHTML tracks the latest Zig release (0.13.0 at the moment of writing). 
+
+### Contributing to the HTML paser & LSP
 Contributing to the HTML parser and LSP doesn't require you to be familiar with the templating language, basically limiting the scope of what you have to worry about to:
 
 - `src/cli.zig`
@@ -19,4 +46,3 @@ Running `zig build` will compile the Super CLI tool, allowing you to also then t
 
 The LSP will log in your cache directory so you can `tail -f ~/.cache/super/super.log` to see what happens with the LSP.
 
-NOTE: while the correct thing to do in terms of logging is to use `std.log`, those lines unfortunately will not be printed while running tests, which is why some times you will see `std.debug.print` be used instead of `log.debug`. Ideally all non-logging prints should be removed from the codebase at some point.
--- build.zig
+++ build.zig
@@ -75,4 +75,55 @@ pub fn build(b: *std.Build) !void {
 
     const check = b.step("check", "Check if Super compiles");
     check.dependOn(&super_cli_check.step);
+
+    const release_step = b.step("release", "Create releases for the Super CLI tool");
+    const targets: []const std.Target.Query = &.{
+        .{ .cpu_arch = .aarch64, .os_tag = .macos },
+        .{ .cpu_arch = .aarch64, .os_tag = .linux },
+        .{ .cpu_arch = .x86_64, .os_tag = .macos },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+        .{ .cpu_arch = .x86_64, .os_tag = .windows },
+        .{ .cpu_arch = .aarch64, .os_tag = .windows },
+    };
+
+    for (targets) |t| {
+        const release_target = b.resolveTargetQuery(t);
+
+        const super_exe_release = b.addExecutable(.{
+            .name = "super",
+            .root_source_file = b.path("src/cli.zig"),
+            .target = release_target,
+            .optimize = .ReleaseFast,
+        });
+
+        const release_mode = .{ .target = release_target, .optimize = .ReleaseFast };
+
+        const scripty_release = b.dependency("scripty", release_mode);
+        const ts_release = b.dependency("tree-sitter", release_mode);
+
+        const super_release = b.addModule("super", .{
+            .root_source_file = b.path("src/root.zig"),
+            .link_libc = true,
+        });
+
+        super_release.addImport("scripty", scripty_release.module("scripty"));
+        super_release.addImport("treez", ts_release.module("treez"));
+        super_release.linkLibrary(ts_release.artifact("tree-sitter"));
+        super_exe_release.root_module.addImport("super", super_release);
+        super_exe_release.root_module.addImport(
+            "known-folders",
+            folders.module("known-folders"),
+        );
+        super_exe_release.root_module.addImport("lsp", lsp.module("lsp"));
+
+        const target_output = b.addInstallArtifact(super_exe_release, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try t.zigTriple(b.allocator),
+                },
+            },
+        });
+
+        release_step.dependOn(&target_output.step);
+    }
 }
