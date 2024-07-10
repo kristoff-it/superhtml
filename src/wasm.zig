97665fcd0f4f1fbac6613face127daf2ac8ebc4c
Loris Cro <kappaloris@gmail.com>
2024-07-10 19:43:17 +0200

    more wasm experimentation

--- src/wasm.zig
+++ src/wasm.zig
@@ -6,12 +6,11 @@ const lsp_exe = @import("cli/lsp.zig");
 pub const version = "0.1.4";
 
 pub fn main() !void {
+    std.debug.print("Yep it werks3", .{});
     // const gpa = std.heap.wasm_allocator;
     // try lsp_exe.run(gpa, &.{});
 
-    std.debug.print("Yep it werks2", .{});
-
-    const Buf = std.fifo.LinearFifo(u8, .{ .Static = 2048 });
+    const Buf = std.fifo.LinearFifo(u8, .{ .Static = 1024 * 128 });
     var buf = Buf.init();
     try buf.pump(std.io.getStdIn().reader(), std.io.getStdErr().writer());
 }
