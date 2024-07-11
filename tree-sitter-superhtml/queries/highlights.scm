3e25ef59622eb05421efb39370bb893874cdcbbb
Loris Cro <kappaloris@gmail.com>
2024-07-12 01:47:55 +0200

    superhtml: improve highlight queries

--- tree-sitter-superhtml/queries/highlights.scm
+++ tree-sitter-superhtml/queries/highlights.scm
@@ -6,6 +6,48 @@
   (#any-of? @special "super" "extend"))
 (tag_name) @tag
 
+; (
+;   (element
+;     (start_tag
+;       (tag_name) @special))
+;   (element
+;     (start_tag
+;       (attribute
+;         (attribute_name) @attribute
+;         [
+;           (attribute_value) @markup.link.url
+;           (quoted_attribute_value (attribute_value) @markup.link.url)
+;         ])))
+;   (#eq? @special "extend")
+;   (#eq? @attribute "id")
+; )
+
+
+(
+  (element
+    (start_tag
+      (attribute 
+        (attribute_name) @attribute
+        [
+          (attribute_value) @markup.link.url
+          (quoted_attribute_value (attribute_value) @markup.link.url)
+        ]))
+    (element
+      (start_tag
+        (tag_name) @tag)))
+  (#eq? @tag "super")
+  (#eq? @attribute "id")
+)
+
+(
+  (element
+    (start_tag
+      (tag_name) @special
+      (attribute
+        (attribute_name) @error)+))
+  (#eq? @special "super")
+)
+
 ; ((attribute_name) @keyword.control.conditional
 ;   (#match? @keyword.control.conditional "^[$]"))
 (attribute_name) @attribute
