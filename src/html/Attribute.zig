a088aa029809b5bcbf8fb5a167360b462693d08a
Andrea Feletto <andrea@feletto.io>
2025-09-16 21:29:06 +0200

    html: lang: use rule for global lang attribute

--- src/html/Attribute.zig
+++ src/html/Attribute.zig
@@ -1797,7 +1797,7 @@ pub const global: AttributeSet = .init(&.{
     .{
         .name = "lang",
         .model = .{
-            .rule = .not_empty,
+            .rule = .lang,
             .desc = "The `lang` global attribute helps define the language of an element: the language that non-editable elements are written in, or the language that the editable elements should be written in by the user. The attribute contains a single BCP 47 language tag.",
         },
     },
