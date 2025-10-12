774f80782af5bf49f1052969fdf17576921e03ac
Loris Cro <kappaloris@gmail.com>
2025-10-12 22:37:33 +0200

    `<link>`: update `[crossorigin]` to use builtin rule
    The `link` element was slightly out of sync with other attributes because
    it defined its own version of `crossorigin`, which also (wrongli) did not
    allow for empty values.

--- src/html/elements/link.zig
+++ src/html/elements/link.zig
@@ -155,19 +155,8 @@ pub const attributes: AttributeSet = .init(&.{
     .{
         .name = "crossorigin",
         .model = .{
+            .rule = .cors,
             .desc = "This enumerated attribute indicates whether CORS must be used when fetching the resource. CORS-enabled images can be reused in the `<canvas>` element without being tainted.",
-            .rule = .{
-                .list = .init(.missing, .one, &.{
-                    .{
-                        .label = "anonymous",
-                        .desc = "A cross-origin request (i.e., with an `Origin` HTTP header) is performed, but no credential is sent (i.e., no cookie, X.509 certificate, or HTTP Basic authentication). If the server does not give credentials to the origin site (by not setting the `Access-Control-Allow-Origin` HTTP header) the resource will be tainted and its usage restricted.",
-                    },
-                    .{
-                        .label = "use-credentials",
-                        .desc = "A cross-origin request (i.e., with an `Origin` HTTP header) is performed along with a credential sent (i.e., a cookie, certificate, and/or HTTP Basic authentication is performed). If the server does not give credentials to the origin site (through `Access-Control-Allow-Credentials` HTTP header), the resource will be tainted and its usage restricted.",
-                    },
-                }),
-            },
         },
     },
     .{
