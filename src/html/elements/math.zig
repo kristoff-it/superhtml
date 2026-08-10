bd099a3f4db8e2255e9da653e218861e75710401
jamiedsmith95 <47906062+jamiedsmith95@users.noreply.github.com>
2026-08-10 15:06:43 +0100

    add 'display' and 'alttext' attributes to <math>, tests (#156)

--- src/html/elements/math.zig
+++ src/html/elements/math.zig
@@ -37,3 +37,73 @@ pub const math: Element = .{
     \\ - [MathML Spec](https://w3c.github.io/mathml-core/#the-top-level-math-element)
     ,
 };
+
+pub const attributes: AttributeSet = .init(&.{
+    .{
+        .name = "display",
+        .model = .{
+            .rule = .{
+                .list = .init(.none, .one, &.{
+                    .{
+                        .label = "block",
+                        .desc = "Element will be displayed in its own block outside the current span of text.",
+                    },
+                    .{
+                        .label = "inline",
+                        .desc = "(default) Element will be displayed inside the current span of text.",
+                    },
+                }),
+            },
+            .desc = "The `display` attribute is an enumerated attribute that specifies how the enclosed MathML markup should be rendered. Defaults to 'inline' if missing or invalid. ",
+        },
+    },
+    .{
+        .name = "alttext",
+        .model = .{
+            .rule = .any,
+            .desc = "The `alttext` attribute provides a text-based description of the mathematical content.",
+        },
+    },
+});
+
+test "math - bad display attribute" {
+    const case = "<math " ++
+    \\display="bad"
+    ++ "></math>\n";
+    const ast = try Ast.init(std.testing.allocator, case, .html, false);
+    defer ast.deinit(std.testing.allocator);
+    try std.testing.expect(1 == ast.errors.len);
+}
+
+test "math - display attribute" {
+    const case = "<math " ++
+    \\display="block"
+    ++ "></math>\n";
+    const ast = try Ast.init(std.testing.allocator, case, .html, false);
+    defer ast.deinit(std.testing.allocator);
+    try std.testing.expect(0 == ast.errors.len);
+}
+test "math - alttext attribute" {
+    const case = "<math " ++
+    \\alttext="x equals y sqaured"
+    ++ "></math>\n";
+    const ast = try Ast.init(std.testing.allocator, case, .html, false);
+    defer ast.deinit(std.testing.allocator);
+    try std.testing.expect(0 == ast.errors.len);
+}
+
+test "math display - completion" {
+    const case = "<math " ++
+    \\display=""
+    ++ "></math>\n";
+    const ast = try Ast.init(std.testing.allocator, case, .html, false);
+    defer ast.deinit(std.testing.allocator);
+
+    var arena: std.heap.ArenaAllocator= .init(std.testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+
+    const comp = try ast.completions(alloc,case, 14);
+    const expected = "inline";
+    try std.testing.expectEqualStrings(expected,comp[1].label);
+    try std.testing.expect(2 == comp.len);}
